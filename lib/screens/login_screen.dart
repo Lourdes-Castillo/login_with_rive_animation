eeecb14ea6463ae22ddb6bcccf5230cd31660932
Lourdes Castillo <castillocaamallourdesmarisol@gmail.com>
2026-02-05 11:01:02 -0600

    feat(rive): animation added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      //Evitar que se quite el esapacio del nudge
+      body: SafeArea(
+        child:Column(
+          children: [
+            Expanded(
+              child: RiveAnimation.asset('animated_login_bear.riv'))
+          ],
+        ))
+
+    );
+  }
+}
