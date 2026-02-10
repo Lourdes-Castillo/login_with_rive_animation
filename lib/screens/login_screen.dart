53741644f0d348c824fdc4b896e6f945ccafaf0b
Lourdes Castillo <castillocaamallourdesmarisol@gmail.com>
2026-02-10 13:29:38 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,16 +9,64 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //Controlar para mostra o ocultar la contraseña
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    //Para obtener el tamaño de la memoria 
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
       //Evitar que se quite el esapacio del nudge
       body: SafeArea(
-        child:Column(
-          children: [
-            Expanded(
-              child: RiveAnimation.asset('animated_login_bear.riv'))
-          ],
+        child:Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: const RiveAnimation.asset('animated_login_bear.riv'),
+              ),
+              const SizedBox(height: 18),
+              TextField(
+                //Para un tipo de yeclafo
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12)  
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10,),
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  hintText: 'Password' ,
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off
+                      ),
+                      onPressed: () {
+                        //Refrescar el Icono
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10,),
+              TextField(
+              )
+            ],
+          ),
         ))
 
     );
